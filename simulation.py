22ca5353261d6808d06aac98896af20300aca4cd
uday-31 <77710794+uday-31@users.noreply.github.com>
2021-08-30 02:22:45 +0800

    clean up, add examples

--- simulation.py
+++ simulation.py
@@ -6,12 +6,18 @@ import pickle
 from tqdm.notebook import tqdm
 
 
-def runSimulations(numberOfPlayers: int, numberOfSimulations: int, append: dict = None) -> dict:
+def runSimulations(numberOfPlayers: int, numberOfSimulations: int, append: dict = None, verbose: bool = True) -> dict:
 
     if append is None:
         simulations = {item:np.array([0,0,0]) for item in Game.HAND_RANKINGS.keys()}
     else:
         simulations = append
+    
+    if(verbose):
+        if append is None:
+            print(f"Running {numberOfSimulations} simulations for a game of {numberOfPlayers} players")
+        else:
+            print(f"Adding {numberOfSimulations} simulations for a game of {numberOfPlayers} players")
 
     for _ in tqdm(range(numberOfSimulations)):
         game = Game(numberOfPlayers)
@@ -21,13 +27,16 @@ def runSimulations(numberOfPlayers: int, numberOfSimulations: int, append: dict 
 
     return simulations
 
-def saveRaw(results: dict, numberOfPlayers: int):
+def saveRaw(results: dict, numberOfPlayers: int, verbose: bool = True):
 
     fileName = 'results/winrate_'+str(numberOfPlayers)+"_raw.pickle"
     with open(fileName, 'wb') as handle:
         pickle.dump(results, handle, protocol=pickle.HIGHEST_PROTOCOL)
+    
+    if(verbose):
+        print(f"Raw data file saved at {fileName}!")
 
-def clean(results: dict, numberOfPlayers: int) -> pd.DataFrame:
+def clean(results: dict, numberOfPlayers: int, verbose: bool = True) -> pd.DataFrame:
 
     names = list(Game.HAND_NAMES.values())
 
@@ -38,23 +47,37 @@ def clean(results: dict, numberOfPlayers: int) -> pd.DataFrame:
     cleaned['winrate'] = round(cleaned['winrate'], 4)
     cleaned.index = [", ".join(list(item)) for item in cleaned.index]
 
+    if(verbose):
+        print("Converted dictionary to DataFrame and cleaned it!")
+
     return cleaned
 
-def saveCleaned(results: pd.DataFrame, numberOfPlayers: int):
+def saveCleaned(results: pd.DataFrame, numberOfPlayers: int, verbose: bool = True):
 
     fileName = 'results/winrate_'+str(numberOfPlayers)+"_cleaned.csv"
     results.to_csv(fileName)
 
-def loadRaw(numberOfPlayers: int) -> dict:
+    if(verbose):
+        print(f"Cleaned data file saved at {fileName}!")
+
+def loadRaw(numberOfPlayers: int, verbose: bool = True) -> dict:
 
     fileName = 'results/winrate_'+str(numberOfPlayers)+"_raw.pickle"
     with open(fileName, 'rb') as handle:
         res = pickle.load(handle)
 
+    if(verbose):
+        print(f"Data loaded from {fileName}!")
+    
     return res
 
-def simulationsRun(numberOfPlayers: int):
+def simulationsRun(numberOfPlayers: int, verbose: bool = True):
 
     df = pd.read_csv('results/winrate_'+str(numberOfPlayers)+"_cleaned.csv")
-    return int(df['total_games'].sum()/numberOfPlayers)
+    num = int(df['total_games'].sum()/numberOfPlayers)
+
+    if(verbose):
+        print(f"{num} simulations have been run for {numberOfPlayers} players")
+    else:
+        return num
 
